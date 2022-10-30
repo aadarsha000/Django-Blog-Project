f8fc6d260ffdb209df00b02686f0951476f91bf0
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-30 15:33:55 +0545

    list and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def postlist(request):
+    posts = Post.objects.filter(status="published")
+    return render(request, 'index.html', {"posts":posts})
+
+def postdetail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'detail.html', {"post":post})
