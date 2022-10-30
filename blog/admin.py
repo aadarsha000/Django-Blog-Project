bb886dc3755e6458b7f297230f4f6475ff0e0ad4
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-30 15:33:35 +0545

    style admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author','category' ,'status']
+    list_filter = ['status', 'author']
+    search_fields = ['title', 'author', 'category']
+    prepopulated_fields = {"slug":("title",)}
+    raw_id_fields = ["author"]
+
+admin.site.register(Post, PostAdmin)
