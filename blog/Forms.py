26b9ff5c74e263a6538b1006204b01b498373fb5
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-30 15:11:35 +0545

    created form

---
+++ blog/Forms.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from tkinter.tix import Form
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title','category', 'body']
