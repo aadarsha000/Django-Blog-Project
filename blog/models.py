627d2a70ffdc38bd74f292ffa235f1effbb5c927
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-30 15:34:15 +0545

    category

--- blog/models.py
+++ blog/models.py
@@ -11,11 +11,16 @@ class Post(models.Model):
         ("Life style", "Life style"),
         ("History", "History"),
     )
+    Status_choice=(
+        ("Draft", "Draft"),
+        ('published', 'published')
+    )
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
-    category = models.CharField(max_length=100, )
+    category = models.CharField(max_length=100, choices=Category_choice)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    status = models.CharField(max_length=100, choices=Status_choice, default="Draft")
     published = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now = True)
