35baf4a269609d9c07c041ccc45d127f9a7bcbbc
aadarsha000 <116047355+aadarsha000@users.noreply.github.com>
2022-10-31 09:57:23 +0545

    add media url

--- myblog/urls.py
+++ myblog/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls',namespace="blog")),
+    path('account/',include('account.urls',namespace="account")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
